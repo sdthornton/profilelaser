3a59a1847560241472ae7bf707f9ccd986e0627f
Sam Thornton <sdthornton@live.com>
2013-02-06 10:59:41 -0800

    Temporarily removed GA script until site is live.

--- _footer.php
+++ _footer.php
@@ -39,10 +39,11 @@
   <script src="js/plugins.js"></script>
   <script src="js/main.js"></script>
 
-  <!-- Google Analytics -->
+  <!-- Google Analytics
   <script>
     var _gaq=[['_setAccount','UA-37457373-1'],['_trackPageview']];
     (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
     g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
     s.parentNode.insertBefore(g,s)}(document,'script'));
   </script>
+  -->
